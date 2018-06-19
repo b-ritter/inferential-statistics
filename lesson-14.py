a15f2359be424e30d9688796d0060098e39c642a
Ben Ritter (br440r) <br440r@att.com>
2018-06-18 21:28:53 -0500

    Start on lesson 14: ANOVA

---
+++ lesson-14.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+food_a = np.array([2, 4, 3])
+
+food_b = np.array([6, 5, 7])
+
+food_c = np.array([8, 9, 10])
+
+food_gm = np.array(food_a + food_b + food_c).mean()
+
+# print(food_gm)
+# for item in [('a',food_a), ('b',food_b), ('c',food_c)]:
+#   print(f'{item[0]} mean is {np.array(item[1]).mean()}')
+
+# a mean is 3.0
+# b mean is 6.0
+# c mean is 9.0
+
+foods = {
+  'a': food_a,
+  'b': food_b,
+  'c': food_c
+}
+for food, weights in zip(foods.keys(),foods.values()):
+  print(f'{food} sum of squares: {np.sum(np.square(weights - weights.mean()))}')
